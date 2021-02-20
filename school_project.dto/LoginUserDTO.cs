6cb3e12bca9c36abc01d5a42e1c222df07a5acd5
Dhanesh Kumar <Dhanesh-Kumar@hotmail.com>
2021-02-20 11:52:29 +0530

    creating a login page, creating dto

---
+++ school_project.dto/LoginUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace school_project.dto
+{
+    public class LogiUserDTO
+    {
+        public string user_name { get; set; }
+        public string user_password { get; set; }
+        public string user_role { get; set; }
+        public string company_code { get; set; }
+        public bool is_remember { get; set; }
+    }
+
+}
